5797f1aa8fd3902f851f03f13234ab9bbd7e88cb
nazhansukri11 <nazhansukri11@gmail.com>
2023-12-03 13:04:58 +0800

    button and show alert exercise

---
+++ src/components/ButtonComponent.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+interface Props{
+    color?:'primary' | 'secondary';
+    children:string;
+    handleClick:()=>void;
+}
+
+const ButtonComponent = ({color="primary",children, handleClick}:Props) => {
+   
+
+  return (
+    <button className={"btn btn-" + color} onClick={handleClick}>{children}</button>
+  )
+}
+
+export default ButtonComponent
